566c7b5711d09d5a78c2ea69ccbc6f81aafadbbc
Shalin Scupham <shalin.scupham@gmail.com>
2018-01-02 13:30:23 -0500

    back end auth working in console

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  helper_method :current_user, :signed_in?
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def login(user)
+    user.reset_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  # def require_signed_in
+  #   unless current_user
+  #     render json: { base: ['invalid credentials'] }, status: 401 }
+  #   end
+  # end
+
+  def logout
+    current_user.try(:reset_token)
+    session[:session_token] = nil
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  validates :username, :session_token, :password_digest, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  after_initialize :ensure_session_token
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username,password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+end
