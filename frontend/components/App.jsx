32014e629c48b726b0ecf5aa38e39fee65ef28c2
Shalin Scupham <shalin.scupham@gmail.com>
2018-01-02 14:25:53 -0500

    App component rendering in entry via root component

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Sideshow™: now rendering via App Ccomponent</h1>
+  </div>
+);
+
+export default App;
--- frontend/sideshow.jsx
+++ frontend/sideshow.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root'
 import {signup, login, logout} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
+
+// TESTING START
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
-  ReactDOM.render(<h1>Welcome to Sideshow</h1>, root);
+// TESTING END
+
+  ReactDOM.render(<Root store={ store } />, root);
 
 });
