99efaa397dfdc536c4217c996e4330e37db57320
Shalin Scupham <shalin.scupham@gmail.com>
2018-01-04 17:42:16 -0500

    Stub out events controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :events, only: [:index, :create, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
