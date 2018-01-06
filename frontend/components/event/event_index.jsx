d736c0216047e2f32e18875bc2373cd977d55cbd
Shalin Scupham <shalin.scupham@gmail.com>
2018-01-06 13:43:02 -0500

    Continue refining homepage / event index css

--- frontend/components/event/event_index.jsx
+++ frontend/components/event/event_index.jsx
@@ -17,24 +17,30 @@ class EventIndex extends React.Component {
           event={event}
           key={event.id}
         />
-      ));
+    ));
     }
 
     return (
-      <div>
-        <div className="home-hero-image">
-          <img src="https://cdn.glitch.com/a5121e34-96b3-4a70-8227-040c51e64fae%2Fbg.jpg?1509627430438" />
-        </div>
-        <div className="welcome-box">
-          <h1 className="homepage-header">Find your next experience</h1>
-        </div>
-        <div className="ui-text-subhead">
-          Events for you in Brooklyn, NY
-        </div>
-        <section className="cards">
-          {content}
+      <section className="home-flex-box">
+        <div className="home-hero-image"></div>
+
+        <section className="home-content">
+
+          <article className="welcome-box">
+            <h1 className="homepage-header">Find your next experience</h1>
+          </article>
+
+          <article className="ui-text-subhead">
+            <h3 className="text-subhead">Events for you in Brooklyn, NY</h3>
+          </article>
+
+          <section className="cards">
+            {content}
+          </section>
+
         </section>
-      </div>
+
+    </section>
     );
   }
 }
