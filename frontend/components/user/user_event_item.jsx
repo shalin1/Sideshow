0e3c2a753ff17486ca3b5c91a953bd85fd11d0d6
Shalin Scupham <shalin.scupham@gmail.com>
2018-01-12 05:48:36 -0500

    text styling on user event show page

--- frontend/components/user/user_event_item.jsx
+++ frontend/components/user/user_event_item.jsx
@@ -18,28 +18,28 @@ class UserEvents extends React.Component {
       id, title, event_start, tickets_available, tickets, event_show_url
     } = this.props.event;
 
-    const eventStart = moment(event_start).format("ddd, MMM D h:mm A");
+    const eventStart = moment(event_start).format("MMM D YYYY h:mm A");
 
     return (
       <article className="user-dashboard-ownevents-item-container">
-        <div className="user-dashboard-ownevents-item-date">
-          {eventStart}
-        </div>
         <div className="user-dashboard-ownevents-item-title">
           {title}
         </div>
+        <div className="user-dashboard-ownevents-item-date">
+          {eventStart}
+        </div>
         <div className="user-dashboard-ownevents-item-actions">
           <Link to={`/events/${id}`}>
-            View<br/>Event
+            <i class="fa fa-eye" aria-hidden="true"> </i>
+            View Event
           </Link>
           <Link to={`/events/${id}/edit`}>
-            Edit<br/>Event
+            <i class="fa fa-pencil" aria-hidden="true"> </i>
+            Edit Event
           </Link>
-          <button
-            className="user-dashboard-button"
-            onClick={this.handleDelete}
-          >
-            Delete<br/>Event
+          <button onClick={this.handleDelete} >
+            <i className="fa fa-trash-o" aria-hidden="true"> </i>
+            Delete Event
           </button>
         </div>
       </article>
