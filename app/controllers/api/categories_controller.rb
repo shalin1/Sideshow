894ac396afffa924f0291cea1e037fd171a2a4dc
Shalin Scupham <shalin.scupham@gmail.com>
2018-01-24 21:17:28 -0500

    Create rails controller for Categories & restful routing in Rails. Create placeholder files for jbuider views and category API util

---
+++ app/controllers/api/categories_controller.rb
@@ -0,0 +1,51 @@
+class Api::CategoriesController < ApplicationController
+
+    def index
+      @categories = Category.all
+    end
+
+    def show
+      @category = Category.find(params[:id])
+      if @category
+        render :show
+      else
+        render json: ['Could not find category'], status: 404
+      end
+    end
+
+    def create
+      @category = Category.new(catagory_params)
+      if @category.save
+        @user = current_user
+        render :show
+      else
+        render json: @category.errors.full_messages, status: 422
+      end
+    end
+
+    def update
+      @category = current_user.events.find(params[:id])
+      if @category.update_attributes(event_params)
+        render :show
+      else
+        render json: @event.errors.messages, status: 422
+      end
+    end
+    
+    def destroy
+      @category = Category.find(category_params[:id])
+      if @category.destroy
+        render :index
+      else
+        render json: @category.errors.full_messages, status: 422
+      end
+    end
+
+    def
+
+    private
+
+    def category_params
+      params.require(:category).permit(:id, :name, :category_image)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
-    resources :events
+    resources :events, except: [:new, :edit]
+    resources :categories, except: [:new, :edit]
     resources :tickets, only: [:create, :destroy, :show, :index]
     resources :bookmarks, only: [:create, :destroy]
   end
