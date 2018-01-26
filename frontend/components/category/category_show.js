332df802e86f4ce0b44cee7636e8b7fdf390c610
Shalin Scupham <shalin.scupham@gmail.com>
2018-01-26 03:00:43 -0500

    May have broken it...

--- frontend/components/category/category_show.js
+++ frontend/components/category/category_show.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import moment from 'moment';
 
 class CategoryShow extends React.Component {
+  constructor(props) {
+    super(props);
+    const categoryId = this.props.match.params.id
+  }
+
   componentWillMount() {
-    this.props.fetchCategory(this.props.categoryId).then(console.log('events fetched'));
+    this.props.pageIsLoading();
+    this.props.fetchCategory(this.props.match.params.id).then(
+      () => this.props.pageFinishedLoading()
+    )
+    window.scrollTo(0,0);
   }
 
   render() {
-    let category_show_items;
-    if (!this.props.entities.events) {
-      category_show_items = <h1>loading...</h1>
+    if (this.props.loading) {
+      console.log('loading...');
+      return (
+        <h1>loading....</h1>
+      );
     } else {
-      console.log('category.show working!')
-      category_show_items = <h1>Console show render is here!'</h1>;
+      let events = this.props.categories.categoryId.events;
+      console.log (`here's ${events}`)
+      return(<h1>foo</h1>)
     }
 
-    return category_show_items
   }
 };
 
-export default (CategoryShow);
+export default withRouter(CategoryShow);
--- frontend/components/category/category_show_container.js
+++ frontend/components/category/category_show_container.js
@@ -2,21 +2,25 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { fetchCategory, fetchCategories } from '../../actions/category_actions';
 import { addBookmark, removeBookmark } from '../../actions/session_actions';
+import { pageIsLoading, pageFinishedLoading } from '../../actions/loading_toggle_actions';
 import CategoryShow from './category_show';
 
 const mapStateToProps = (state, { match }) => {
-  const categoryId = parseInt(match.params.id);
+  const categories = Object.values(state.entities.categories);
   const currentUser = state.session.currentUser;
   return {
-    categoryId,
-    errors: state.errors.category
+    categories,
+    currentUser,
   }
 };
 
 const mapDispatchToProps = dispatch => ({
   fetchCategory: categoryId => dispatch(fetchCategory(categoryId)),
+  fetchCategories: () => dispatch(fetchCategory()),
   addBookmark: bookmark => dispatch(addBookmark(bookmark)),
   removeBookmark: eventId => dispatch(removeBookmark(eventId)),
+  pageIsLoading: () => dispatch(pageIsLoading()),
+  pageFinishedLoading: () => dispatch(pageFinishedLoading()),
 })
 
 export default connect (
